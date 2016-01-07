fa7b07c2b27104fac49b1130e39bcdb117ef4478
Helge Rausch <helge@rausch.io>
2016-01-07 18:01:55 +0100

    Use OpenSSL::X509::DEFAULT_CERT_FILE

--- spec/support/setup_api_client.rb
+++ spec/support/setup_api_client.rb
@@ -6,7 +6,7 @@ module SetupApiClient
       config.username = ENV.fetch('EASYBILL_USERNAME')
       config.password = ENV.fetch('EASYBILL_API_KEY')
       # config.debugging = true
-      config.ssl_ca_cert = '/etc/ssl/certs/ca-certificates.crt'
+      config.ssl_ca_cert = OpenSSL::X509::DEFAULT_CERT_FILE
     end
     @api_client = Easybill::ApiClient.new(config)
   end
