b57507e05eb3082ae1ae23ea1fabaa8a430c293b
Roman Kushnir <roman.kushnir@ad2games.com>
2016-06-10 16:42:53 +0200

    Bring customer API specs to the common view

--- spec/integration/customer_api_spec.rb
+++ spec/integration/customer_api_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 module EasybillRestClient
   RSpec.describe CustomerApi, :vcr do
-    let(:api) { client.customers }
+    subject { client.customers }
 
     describe 'a list of customers' do
       it 'gets all customers' do
-        customers = api.find_all.to_a
+        customers = subject.find_all.to_a
         expect(customers).not_to be_empty
         customers.each do |customer|
           expect(customer).to be_a(Customer)
@@ -13,32 +13,32 @@ module EasybillRestClient
       end
 
       it 'gets customers matching a filter' do
-        expect(api.find_all(number: '15408').to_a).to match_array [
+        expect(subject.find_all(number: '15408').to_a).to match_array [
           an_object_having_attributes(number: '15408')
         ]
       end
     end
 
     it 'creates, updates and deletes customer' do
-      existing_customer = api.find_all(number: 'TEST').first
+      existing_customer = subject.find_all(number: 'TEST').first
       if existing_customer
-        api.delete(existing_customer.id)
-        expect(api.find_all(number: 'TEST').to_a).to be_empty
+        subject.delete(existing_customer.id)
+        expect(subject.find_all(number: 'TEST').to_a).to be_empty
       end
 
-      customer = api.create(
+      customer = subject.create(
         number: 'TEST',
         company_name: 'Fake',
         first_name: 'John',
         last_name: 'Doe')
 
-      api.update(customer.id, first_name: 'Bob')
+      subject.update(customer.id, first_name: 'Bob')
 
-      customer = api.find(customer.id)
+      customer = subject.find(customer.id)
       expect(customer.first_name).to eq('Bob')
 
-      api.delete(customer.id)
-      expect(api.find_all(number: 'TEST').to_a).to be_empty
+      subject.delete(customer.id)
+      expect(subject.find_all(number: 'TEST').to_a).to be_empty
     end
   end
 end
